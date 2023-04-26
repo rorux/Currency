63507ffcc03b5210e4d75d576b814d110f8cb584
RoRux <rorux84@gmail.com>
2023-04-26 16:06:43 +0300

    feat(pages\settings): add new chart to store

--- src/pages/settings-page/new-chart-dialog/new-chart-dialog.tsx
+++ src/pages/settings-page/new-chart-dialog/new-chart-dialog.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { Button, Col, Row } from "react-bootstrap";
+import { useAppDispatch } from "@store";
+import { addChart } from "@features/charts";
 import { Dialog, Select } from "@components";
 import { ChartColorPicker } from "../chart-color-picker";
 import { useCurrency } from "./use-currency";
@@ -9,11 +12,25 @@ export const NewChartDialog = ({
   show,
   setShow,
 }: NewChartDialogProps): React.ReactElement => {
+  const dispatch = useAppDispatch();
   const [lineColor, setLineColor] = useState("#888");
   const [fillColor, setFillColor] = useState("#888");
   const { selectedCurrency, currencies, changeCurrency } = useCurrency();
 
   const handleClose = () => setShow(false);
+
+  const handleClick = () => {
+    if (!selectedCurrency) {
+      toast.error("Choose a currency!");
+    } else {
+      const newChart = {
+        currency: selectedCurrency,
+        lineColor,
+        fillColor,
+      };
+      dispatch(addChart(newChart));
+    }
+  };
 
   return (
     <Dialog title="New Chart Settings" show={show} handleClose={handleClose}>
@@ -36,7 +53,7 @@ export const NewChartDialog = ({
       </Row>
 
       <div className="text-center mb-2">
-        <Button variant="primary" className="mt-4">
+        <Button variant="primary" className="mt-4" onClick={handleClick}>
           Add chart
         </Button>
       </div>
